9597d5971de93843330c27c994cdede2dd3faf71
Barry <eragonudoka77@gmail.com>
2023-02-21 19:30:32 -0800

    u

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,17 +1,18 @@
-#include "main.h"
+#include <stdio.h>
 /**
- * print_last_digit - prints the last digit of a number
- * @n: the int to extract the last digit from
- * Return: value of the last digit
+ * main - Lists all the natural numbers below 1024 (excluded)
+ * that are multiples of 3 or 5.
+ *
+ * Return: Always 0.
  */
-int print_last_digit(int n)
+int main(void)
 {
-int a;
-if (n < 0)
-	n = -n;
-	a = n % 10;
-if (a < 0)
-	a = -a;
-_putchar(a + '0');
-	return (a);
+int i, sum = 0;
+for (i = 0; i < 1024; i++)
+{
+if ((i % 3) == 0 || (i % 5) == 0)
+sum += i;
+}
+printf("%d\n", sum);
+return (0);
 }
